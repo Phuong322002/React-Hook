3c80ed94f1e41dacfc4e009166b7c807a72d17c0
PhuongVu <puong334@gmail.com>
2024-05-05 22:42:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
+import Header from './Component/Header';
 
 
 const App = () => {
 
   return (
     <div className="app-container">
-        hello
+        <Header/>
     </div>
   );
 }
