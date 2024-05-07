0e0e075ed43d30c0a59749729a843f7581a0ec8a
PhuongVu <puong334@gmail.com>
2024-05-07 15:09:01 +0700

    update

--- src/Component/Home/HomePage.jsx
+++ src/Component/Home/HomePage.jsx
@@ -8,6 +8,15 @@ const HomePage = () => {
             <video autoPlay muted loop >
                 <source src={videoHomepage} />
             </video>
+            <div className="content-container">
+                <div className="title">There's a better way to ask</div>
+                <div className="text"> You don't want to make a boring form. And your audience won't answer one.
+                    Create a typeform instead-and make everyone happy</div>
+                <div>
+                    <button className="btn-start">Get's started. It's fress</button>
+                </div>
+            </div>
+
         </div>
     )
 }
