e2e0c2ce306fc6a3a22bc030342c090dbba3e998
PhuongVu <puong334@gmail.com>
2024-05-24 21:49:35 +0700

    demoRedux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import UserComponent from './Component/UserInfor';
@@ -10,14 +10,14 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-      <UserComponent/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
+      {/* <UserComponent/> */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button> */}
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
       </header>
     </div>
   );
--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import counterReducer from './counterReducer';
 
 const rootReducer = combineReducers({
-    counter: counterReducer,
+    // counter ở đây theo tôi hiểu nó cũng là state của hàm counterReducer
+    counter: counterReducer
 });
-
 export default rootReducer;
